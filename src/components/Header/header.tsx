81c5acdd04326a434f32458dab664103a1cda03f
CaioFragoso23 <caiocesarfragoso@gmail.com>
2024-03-22 00:03:20 -0300

    Adding Header Component

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,28 +1,58 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { IoPerson, IoCart, IoHeart, IoSearch } from "react-icons/io5";
-import { LeituraHeader, LeituraSearch, LeituraNav } from "./styles";
+import { LeituraHeader, LeituraSearch, LeituraNav, Dropdown, AccountContent } from "./styles";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <LeituraHeader>
-        <Image src={"/LeituraLogo.svg"} width={180} height={30} alt="Leitura" />
-        <LeituraSearch>
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Pesquisar por título, autor, editora ou código de barras"
-          />
-          <button>
-            <IoSearch color="white" />
-          </button>
-        </LeituraSearch>
-        <LeituraNav>
+      <Image src={"/LeituraLogo.svg"} width={180} height={30} alt="Leitura" />
+      <LeituraSearch>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Pesquisar por título, autor, editora ou código de barras"
+        />
+        <button>
+          <IoSearch color="white" />
+        </button>
+      </LeituraSearch>
+      <LeituraNav>
+        <button onClick={() => router.push("/account")}>
           <IoPerson color="#00569D" size="30" />
-          <IoHeart color="#00569D" size="30" />
-          <IoCart color="#00569D" size="30" />
-        </LeituraNav>
-      </LeituraHeader>
+          <Dropdown>
+            <AccountContent>
+              <a>Minha Conta</a>
+              <a>Configurações</a>
+              <a>Meus Pedidos</a>
+              <a>Sair</a>
+            </AccountContent>
+          </Dropdown>
+        </button>
+        <button onClick={() => router.push("/wishlist")}>
+          <IoHeart
+            onClick={() => router.push("/wishlist")}
+            color="#00569D"
+            size="30"
+          />
+          <Dropdown>
+            <p>Books</p>
+          </Dropdown>
+        </button>
+        <button onClick={() => router.push("/cart")}>
+          <IoCart
+            onClick={() => router.push("/cart")}
+            color="#00569D"
+            size="30"
+          />
+          <Dropdown>
+            <p>Cart</p>
+          </Dropdown>
+        </button>
+      </LeituraNav>
+    </LeituraHeader>
   );
 }
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -18,6 +18,27 @@ export const LeituraNav = styled.nav`
   display: flex;
   gap: 16px;
   align-items: center;
+
+  button {
+    border: none;
+    display: inline-block;
+    position: relative;
+    background-color: transparent;
+    padding: 6px 8px;
+  }
+
+  button:hover {
+    background-color: #f9f9f9;
+    border-radius: 8px;
+    box-shadow: 0px -4px 16px 0px rgba(0, 0, 0, 0.2);
+  }
+
+  button:hover div {
+    display: block;
+    /* flex-direction: column;
+    align-items: start;
+    justify-content: center; */
+  }
 `;
 
 export const LeituraSearch = styled.div`
@@ -42,13 +63,36 @@ export const LeituraSearch = styled.div`
     position: absolute;
     background-color: #00569d;
     border: none;
-    border-radius:50%;
+    border-radius: 50%;
     transform: translateX(-20%);
     right: 0;
     height: 90%;
     aspect-ratio: 1;
   }
 `;
+export const Dropdown = styled.div`
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #f9f9f9;
+  min-width: 100px;
+  box-shadow: 0px 12px 16px -6px rgba(0, 0, 0, 0.2);
+  border-radius: 8px 0 8px 8px;
+  padding: 12px 16px;
+  z-index: 1;
+`;
 
-
-
+export const AccountContent = styled.div`
+  width: 100%;
+  
+  a {
+    color: black;
+    text-decoration: none;
+    display: block;
+    padding: 12px 16px;
+  }
+  a:hover {
+    background-color: #00569d;
+    color: white;
+  }
+`;
